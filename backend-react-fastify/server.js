be0999029bfc491642ee9c8fbf4b57a21a685c61
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-02-03 05:11:06 +0500

    setup, part-2, prettier, and eslint

--- backend-react-fastify/server.js
+++ backend-react-fastify/server.js
@@ -1,10 +1,10 @@
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || "0.0.0.0"; // Ensures it listens on all network interfaces
+const HOST = process.env.HOST || '0.0.0.0'; // Ensures it listens on all network interfaces
 
-const server = require("./src/app")({
+const server = require('./src/app')({
   logger: {
     transport: {
-      target: "pino-pretty",
+      target: 'pino-pretty',
     },
   },
 });
@@ -12,9 +12,7 @@ const server = require("./src/app")({
 async function start() {
   try {
     await server.listen({ port: PORT, host: HOST }); // Pass an object
-    console.log(
-      `🚀 Server is running at http://localhost:${PORT}`
-    );
+    console.log(`🚀 Server is running at http://localhost:${PORT}`);
   } catch (error) {
     server.log.error(`Failed to run server at PORT ${PORT}\n${error}`);
     process.exit(1);
