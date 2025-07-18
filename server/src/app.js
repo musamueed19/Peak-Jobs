9188bb1d55adafbaca277c3ff7da1077f56d27fc
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-07-18 10:21:37 +0500

    replace "type": "module"

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,4 @@
-const fastify = require("fastify");
+import fastify from "fastify"
 
 function build(opts = {}) {
   // create fastify app,  with default settings
@@ -11,6 +11,7 @@ function build(opts = {}) {
   return app;
 }
 
-module.exports = build;
+// ES module export
+export default build
 
 // bcz creating a fastify application, and running the server - both are different things.   we can create a fastify app, and then run it in a different file.  or  we can create a fastify app, and then run it in the same file.  it's  up to you.  but in this case, we're just creating a fastify app, and  then returning it.  so that we can use it in another file.  or  we can  run it in the same file, if we want to.  it's up to you.
